26f90732767dee29889efbd4417eb422fccfae2f
shengling <shengling.zhu97@outlook.com>
2026-01-16 14:59:00 +0100

    Add htf-js scripts

---
+++ packages/htf-js/index.js
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2025-2026 ZHU Shengling
+"use strict";
+
+require("./htf/utils");
+require("./htf/signals");
+require("./htf/timeframe");
+require("./htf/features");
+require("./htf/coordinator");
+require("./htf/framework");
+require("./htf/viz/multi_timeframe_plot");
+require("./htf/viz/index");
+require("./htf/index");
+
+module.exports = globalThis.HTF;
